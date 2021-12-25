b06ebcf645be22413a8afe74db8ad8ae9d5b911c
Saetch <nils.wriedt@hotmail.com>
2021-12-26 00:53:16 +0100

    added comment for rotation data field

--- src/model.rs
+++ src/model.rs
@@ -2,6 +2,8 @@ use std::sync::{Arc, Mutex};
 
 use piston::UpdateArgs;
 pub struct Model{
+    //Arc for sharing data between threads (not needed in pistonSquare, as it is singleThreaded, but I wanted to add it, so we don't 
+    //need to change it later), arc-> atomically reference counted. Mutex gives MUTability and is thread safe (sync), see use below
     pub rotation: Arc<Mutex<f64>>
 }
 
