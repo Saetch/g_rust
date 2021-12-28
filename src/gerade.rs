7d6fce49faf76c6fd23e63fcff24202ba6809530
Saetch <nils.wriedt@hotmail.com>
2021-12-28 23:19:28 +0100

    added some functionality to line

--- src/gerade.rs
+++ src/gerade.rs
@@ -2,6 +2,8 @@ pub struct Gerade{
     pub start_punkt: (f64, f64),
     pub linien_vektor: (f64, f64),
     pub end_punkt: (f64, f64),
+    pub normalvektor: Option<(f64, f64)>,       //Option can be None (equivalent to Null in other languages)
+    pub closest_point_to_zerozero: Option<(f64, f64)>
 }
 
 
@@ -13,6 +15,8 @@ impl Gerade{
             start_punkt: punkt,
             linien_vektor: vektor,
             end_punkt: end,
+            normalvektor: None,
+            closest_point_to_zerozero: None,
         }
     }
 
@@ -22,14 +26,40 @@ impl Gerade{
             start_punkt: startpunkt,
             linien_vektor: vektor,
             end_punkt: endpunkt,
+            normalvektor: None,
+            closest_point_to_zerozero: None,
         }
+    }
+
+    pub fn has_normal(&self)-> bool{
+        self.normalvektor.is_some()
     }
 /**
  * this function is supposed to reduce the vektor length to 1,
  * 
  */
     pub fn normalize(&mut self){
+        let veklength = (self.linien_vektor.0*self.linien_vektor.0 + self.linien_vektor.1*self.linien_vektor.1).sqrt();
 
+        self.linien_vektor.0 = self.linien_vektor.0 / veklength;
+        self.linien_vektor.1 = self.linien_vektor.1 / veklength;
+        if !self.has_normal(){
+            self.calculate_normal_vektor();
+        }
+    }
+
+
+    pub fn calculate_normal_vektor(&mut self){
+        let vektor = (-self.linien_vektor.1, self.linien_vektor.0);
+        self.normalvektor = Some(vektor);
+    }
+
+    pub fn only_normalize(&mut self){
+        
+        let veklength = (self.linien_vektor.0*self.linien_vektor.0 + self.linien_vektor.1*self.linien_vektor.1).sqrt();
+
+        self.linien_vektor.0 = self.linien_vektor.0 / veklength;
+        self.linien_vektor.1 = self.linien_vektor.1 / veklength;
     }
 
 }
